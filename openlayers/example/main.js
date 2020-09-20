78bd429a46bf3f7cdc8854828946e9e9b41496fc
Emidio Torre <emidio.torre@gmail.com>
2020-09-20 11:39:58 +0200

    added map to example dir

--- openlayers/example/main.js
+++ openlayers/example/main.js
@@ -3,16 +3,26 @@ import Map from "ol/Map";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
 import { View } from "ol";
+import { FullScreen, defaults as defaultControls } from "ol/control";
+
+const view = new View({
+  center: [41.117143, 16.871871],
+  zoom: 10,
+});
 
 const map = new Map({
+  controls: defaultControls().extend([
+    new FullScreen({
+      source: "fullscreen",
+    }),
+  ]),
   layers: [
     new TileLayer({
       source: new OSM(),
     }),
   ],
   target: "map",
-  view: new View({
-    center: [41.117143, 16.871871],
-    zoom: 10,
-  }),
+  view: view,
 });
+
+view.centerOn([41.117143, 16.871871]);
